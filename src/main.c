ffebc1e9d7489e2ee7bb59eb3d7d3ffbc0488141
Subhankar Mahapatra <mahapatrasubh@outlook.com>
2020-05-26 10:49:33 +0530

    summing function added

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main(int Argc,char *Argv[])
 {
+int sum=0;
+int iterator;
+if(Argc<2)
+{
+printf("Expected sufficient param\n");
+}
+else
+{
+for(iterator=1;iterator<Argc;iterator++)
+{
+sum=sum+atoi(Argv[iterator]);
+}
+printf("sum of your entered munber %d",sum);
+}
 return 0;
 }
